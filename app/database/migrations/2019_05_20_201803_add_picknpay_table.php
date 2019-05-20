901a7d8985ae65bb20ed99971c4c9c4bc7c41199
Hannes <hannesvantonder@crossplatforms.co.za>
2019-05-20 22:23:31 +0200

    added picknpay migration

--- app/database/migrations/2019_05_20_201803_add_picknpay_table.php
+++ app/database/migrations/2019_05_20_201803_add_picknpay_table.php
@@ -5,28 +5,48 @@ use Illuminate\Database\Migrations\Migration;
 
 class AddPicknpayTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('picknpay', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->timestamps();
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('picknpay', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('name');
+			$table->string('store');
+			$table->string('category');
+			$table->dateTime('start_time');
+			$table->dateTime('end_time');
+			$table->string('dwell_time');
+            $table->timestamps();
 		});
-	}
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('picknpay');
-	}
+		$base = Carbon::now();
+		$startDateTimeString = $base->addMinutes(-10)->toDateTimeString();
+		$endDateTimeString = $base->addMinutes(1)->toDateTimeString();
+
+		DB::table('picknpay')->insert([
+			'name' => 'PickNPay',
+			'store' => 'Milnerton',
+			'category' => 'Food',
+			'start_time' => $dateTimeString,
+			'end_time' => $endDateTimeString,
+			'dwell_time' => "100"
+		]);
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('picknpay');
+    }
 
 }
