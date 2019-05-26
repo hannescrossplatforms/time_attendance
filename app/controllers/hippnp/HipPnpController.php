3075b401cac6373194a68542f113753f5d436310
Hannes <hannesvantonder@crossplatforms.co.za>
2019-05-26 17:42:07 +0200

    added changes

--- app/controllers/hippnp/HipPnpController.php
+++ app/controllers/hippnp/HipPnpController.php
@@ -14,7 +14,7 @@ use Session;
 // use App\models\Reportrecipients
 // use BaseController;
 
-class HipPnpController extends \BaseController {
+class HiptnaController extends \BaseController {
 
 
     public function showDashboard($instance = null)
@@ -24,24 +24,29 @@ class HipPnpController extends \BaseController {
         $data = array() ;
         $data['currentMenuItem'] = "Dashboard";
 
-        // if(\Hipauth::hasAllPermissions(array("TNA_CE", "TNA_IM"))) {
-        //     Session::put('availableInstances', "BOTH");
-        // } else if (\Hipauth::hasAnyPermissions(array("TNA_IM"))) {
-        //     Session::put('availableInstances', "IM");
-        //     $instance = "IM";
-        // } else if (\Hipauth::hasAnyPermissions(array("TNA_CE"))) {
-        //     Session::put('availableInstances', "CE");
-        //     $instance = "CE";
-        // } else if (\Hipauth::hasAnyPermissions(array("NR0001","NR0002"))) {
-        //     Session::put('availableInstances', "NR_BOTH");
-        // } else if (\Hipauth::hasAnyPermissions(array("NR0001"))) {
-        //     Session::put('availableInstances', "NR01");
-        //     $instance = "NR01";
-        // } else if (\Hipauth::hasAnyPermissions(array("NR0002"))) {
-        //     Session::put('availableInstances', "NR02");
-        //     $instance = "NR02";
-        // }
-
+        if(\Hipauth::hasAllPermissions(array("TNA_CE", "TNA_IM"))) {
+            Session::put('availableInstances', "BOTH");
+        } else if (\Hipauth::hasAnyPermissions(array("TNA_IM"))) {
+            Session::put('availableInstances', "IM");
+            $instance = "IM";
+        } else if (\Hipauth::hasAnyPermissions(array("TNA_CE"))) {
+            Session::put('availableInstances', "CE");
+            $instance = "CE";
+        } else if (\Hipauth::hasAnyPermissions(array("NR0001","NR0002"))) {
+            Session::put('availableInstances', "NR_BOTH");
+        } else if (\Hipauth::hasAnyPermissions(array("NR0001"))) {
+            Session::put('availableInstances', "NR01");
+            $instance = "NR01";
+        } else if (\Hipauth::hasAnyPermissions(array("NR0002"))) {
+            Session::put('availableInstances', "NR02");
+            $instance = "NR02";
+        }
+        // Session::put('availableInstances', "NR01");
+        // $instance = "NR01";
+        // ANUSHA - CURRENTLY THE ABOVE WILL SET availableInstances to 'BOTH'
+        // TO TEST A SCENARIO WHERE THE USER ONLY HAS ON INSTANCE THEN COMMENT OUT THE NEXT 2 LINES
+        // Session::put('availableInstances', "IM");
+        // $instance = "IM";
 
 
         // if($instance) {
@@ -57,7 +62,7 @@ class HipPnpController extends \BaseController {
         // } else {
 
             Session::put('currentInstance', 'NONE');
-            return \View::make('hippnp.showmaindashboard')->with('data', $data);
+            return \View::make('hiptna.showmaindashboard')->with('data', $data);
 
         // }
     }
