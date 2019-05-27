ac1b29febaa986237e6baea37e4c1850e82bc435
Hannes <hannesvantonder@crossplatforms.co.za>
2019-05-27 20:21:57 +0200

    changes

--- app/controllers/hippnp/HipPnpController.php
+++ app/controllers/hippnp/HipPnpController.php
@@ -1,6 +1,66 @@
 <?php
 
+use DB;
+use Input;
+use Validator;
+use DateTime;
+use Illuminate\Support\Facades\Redirect;
+use Response;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Http\Request;
+use Session;
+
 class hippnp/HipPnpController extends \BaseController {
+
+	public function showDashboard($instance = null)
+    {
+
+
+        $data = array() ;
+        $data['currentMenuItem'] = "Dashboard";
+
+        if(\Hipauth::hasAllPermissions(array("TNA_CE", "TNA_IM"))) {
+            Session::put('availableInstances', "BOTH");
+        } else if (\Hipauth::hasAnyPermissions(array("TNA_IM"))) {
+            Session::put('availableInstances', "IM");
+            $instance = "IM";
+        } else if (\Hipauth::hasAnyPermissions(array("TNA_CE"))) {
+            Session::put('availableInstances', "CE");
+            $instance = "CE";
+        } else if (\Hipauth::hasAnyPermissions(array("NR0001","NR0002"))) {
+            Session::put('availableInstances', "NR_BOTH");
+        } else if (\Hipauth::hasAnyPermissions(array("NR0001"))) {
+            Session::put('availableInstances', "NR01");
+            $instance = "NR01";
+        } else if (\Hipauth::hasAnyPermissions(array("NR0002"))) {
+            Session::put('availableInstances', "NR02");
+            $instance = "NR02";
+        }
+        // Session::put('availableInstances', "NR01");
+        // $instance = "NR01";
+        // ANUSHA - CURRENTLY THE ABOVE WILL SET availableInstances to 'BOTH'
+        // TO TEST A SCENARIO WHERE THE USER ONLY HAS ON INSTANCE THEN COMMENT OUT THE NEXT 2 LINES
+        // Session::put('availableInstances', "IM");
+        // $instance = "IM";
+
+
+        if($instance) {
+
+            Session::put('currentInstance', $instance);
+            if($instance == "NR01" || $instance == "NR02" ) {
+                return Redirect::action('hiptna\HiptnaController@showNrInstanceDashboard');
+            } else {
+                return Redirect::action('hiptna\HiptnaController@showInstanceDashboard');
+            }
+
+
+        } else {
+
+            Session::put('currentInstance', 'NONE');
+            return \View::make('hiptna.showmaindashboard')->with('data', $data);
+
+        }
+    }
 
 	/**
 	 * Display a listing of the resource.
