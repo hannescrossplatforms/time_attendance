e205fd49106abf667229069977071eb0397cace0
Hannes <hannesvantonder@crossplatforms.co.za>
2019-05-29 15:53:59 +0200

    added updates

--- app/views/hippnp/sidebar.blade.php
+++ app/views/hippnp/sidebar.blade.php
@@ -30,16 +30,14 @@
         <li class="li-adm"><a href="{{ url('admin_showdashboard'); }}"><i class="fa fa-gear fa-3x"></i></a></li>
         @endif
         @if (\User::hasAccess("superadmin") || \User::hasAccess("admin"))
-        <!--  <li @if ( $data['currentMenuItem'] == "User Admin" ) class="active" @endif >
-            <a href="{{ url('useradmin_showusers'); }}"><i class="fa fa-user"></i>
-        </li> -->
+
         @endif
         <li><a href="{{ url('support'); }}"><i class="fa fa-phone"></i></a></li>
         <li><a href="{{ url('logout'); }}"><i class="fa fa-unlock"></i></a></li>
     </ul>
 
     <ul class="nav subNav">
-        <!-- <li @if ( $data['currentMenuItem'] == "Dashboard" ) class="active" @endif><a href="{{ url('hiptna_showdashboard'); }}"><i class="fa fa-dashboard"></i>Dashboard</a></li> -->
+
         <div id="instance_menus" style="display: none">
             <ul class="subNav2">
 
@@ -50,18 +48,10 @@
             </ul>
         </div>
         <div id="exception_manage_menus" style="display: none">
-        <!-- @if( Session::get('currentInstance') != "NR01" && Session::get('currentInstance') != "NR02" )
-            <li @if ( $data['currentMenuItem'] == "Exception Reports" ) class="active" @endif ><a href="{{ url('hiptna_exceptionreports'); }}"><i class="fa fa-bell-o"></i>Exception Reports</a></li>
-        @endif -->
-        <!-- <li @if ( $data['currentMenuItem'] == "Management Console" ) class="active" @endif ><a href="{{ url('hiptna_showSettings'); }}"><i class="fa fa-desktop"></i>Management Console</a></li> -->
+
             <ul class="subNav2">
 
-            <!-- <li @if ( $data['currentMenuItem'] == "Settings" ) class="active" @endif ><a href="{{ url('hiptna_showSettings'); }}">Settings</a></li> -->
-            <!-- @if( Session::get('currentInstance') != "NR01" && Session::get('currentInstance') != "NR02" )
-                <li @if ( $data['currentMenuItem'] == "Staff Lookup" ) class="active" @endif ><a href="{{ url('hiptna_showStafflookup'); }}">Staff Lookup</a></li>
-                <li @if ( $data['currentMenuItem'] == "Instant Messaging" ) class="active" @endif ><a href="{{ url('hiptna_showInstantmessaging'); }}">Instant Messaging</a></li>
-                <li @if ( $data['currentMenuItem'] == "Beacon Battery Monitor" ) class="active" @endif ><a href="{{ url('hiptna_showBeaconbatterymonitor'); }}">Beacon Battery Monitor</a></li>
-            @endif -->
+
             </ul>
         </div>
 
