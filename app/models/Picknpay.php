7d7fc8d93dc7d2bfcbf4983a07fa6506be595a2d
Hannes <hannesvantonder@crossplatforms.co.za>
2019-05-29 21:05:13 +0200

    added updates

--- app/models/Picknpay.php
+++ app/models/Picknpay.php
@@ -39,7 +39,7 @@ class Picknpay extends Eloquent {
         ->select('category', DB::raw('sum(dwell_time) dwell_time'))
         ->groupBy('category')
         ->get()->map(function($row){
-            array_push($hannesarray, $row['dwell_time']);
+            array_push($hannesarray, 'asdf');
         });
 
         return $hannesarray;
