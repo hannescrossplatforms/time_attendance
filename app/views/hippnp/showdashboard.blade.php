e2670f23538b06df17ed5165f8f2061c5123b07e
Hannes <hannesvantonder@crossplatforms.co.za>
2019-05-29 15:48:55 +0200

    added updates

--- app/views/hippnp/showdashboard.blade.php
+++ app/views/hippnp/showdashboard.blade.php
@@ -1,9 +1,38 @@
 @extends('layout')
 
 @section('content')
+<style type="text/css">
 
-<body class="hipPnP">
-    <h1>Pick n Pay Dashboard</h1>
+.overlay {
+    background: rgba(129, 119, 119, 0.5) none no-repeat scroll 0% 0%;
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    top: 0px;
+    left: 1px;
+    z-index: 1019;
+    padding-left: 53%;
+    padding-top: 20%;
+}
+</style>
+
+<body class="hipTnA">
+    <div id="loadingDiv" class="overlay">
+        <img src="./img/loader.gif" style="width:80px;">
+    </div>
+    <a id="buildtable"></a>
+
+    <div class="container-fluid">
+        <div class="row">
+
+            @include('hippnp.sidebar')
+
+            <div class="col-sm-9 col-sm-offset-3 col-md-9 col-md-offset-3 main">
+            </div>
+
+
+        </div>
+
 </body>
 
 @stop
