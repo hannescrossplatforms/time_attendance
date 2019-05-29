f74eaf6579d78168f90df8db6d7fced6a9ff6224
Hannes <hannesvantonder@crossplatforms.co.za>
2019-05-29 15:10:44 +0200

    added changes

--- app/controllers/hippnp/HipPnpController.php
+++ app/controllers/hippnp/HipPnpController.php
@@ -15,7 +15,6 @@ class HippnpController extends \BaseController {
 	public function showDashboard($instance = null)
     {
 
-
         $data = array() ;
         $data['currentMenuItem'] = "Dashboard";
 
@@ -36,111 +35,67 @@ class HippnpController extends \BaseController {
             Session::put('availableInstances', "NR02");
             $instance = "NR02";
         }
-        // Session::put('availableInstances', "NR01");
-        // $instance = "NR01";
-        // ANUSHA - CURRENTLY THE ABOVE WILL SET availableInstances to 'BOTH'
-        // TO TEST A SCENARIO WHERE THE USER ONLY HAS ON INSTANCE THEN COMMENT OUT THE NEXT 2 LINES
-        // Session::put('availableInstances', "IM");
-        // $instance = "IM";
-
 
         if($instance) {
+                return Redirect::action('HippnpController@showInstanceDashboard');
+		}
 
-            // Session::put('currentInstance', $instance);
-            // if($instance == "NR01" || $instance == "NR02" ) {
-            //     return Redirect::action('hiptna\HiptnaController@showNrInstanceDashboard');
-            // } else {
-                return Redirect::action('hiptna\HiptnaController@showInstanceDashboard');
-            // }
+	}
 
+	public function showInstanceDashboard()
+    {
+        // Store data in the session...
+        // Session::set('currentInstance', 'IM');
+        $currentInstance = Session::get('currentInstance');
+        $data = array() ;
+        $data['currentMenuItem'] = "Dashboard";
 
-        } else {
+        $settings = $this->getTnaInstanceSettings();
+        $lateness_threshold = \Tnasetting::where('name', 'like', $settings["lateness_threshold"])->first()->value;
+        $proximity_target = \Tnasetting::where('name', 'like', $settings["proximity_target"])->first()->value;
 
-            Session::put('currentInstance', 'NONE');
-            return \View::make('hiptna.showmaindashboard')->with('data', $data);
+        $data['staff_week'] = \Timeandattendance::where('date', ">=", date('Y-m-d',strtotime('last monday')))->where('attendance', '=', "present")->where('instance', '=', $currentInstance )->get()->count();
+        $data['staff_month'] = \Timeandattendance::where('date', ">=", date('Y-m-d',strtotime('first day of this month')))->where('attendance', '=', "present")->where('instance', '=', $currentInstance )->get()->count();
+//        print_r($data);die("here");
+        $cons_absent = \Timeandattendance::select('date')->where('attendance', '!=', "present")->where('instance', '=', $currentInstance )->orderBy('date', 'desc')->first();
+        $start = strtotime($cons_absent->date);
+        $end = strtotime(date('Y-m-d'));
+        $data['cons_absent'] = ceil(abs($end - $start) / 86400)-1;
 
-        }
+        $cons_lateness = \Timeandattendance::select('date')->where('punctuality', '>', $lateness_threshold)->where('instance', '=', $currentInstance)->orderBy('date', 'desc')->first();
+        $start = strtotime($cons_lateness->date);
+        $end = strtotime(date('Y-m-d'));
+        $data['cons_lateness'] = ceil(abs($end - $start) / 86400)-1;
+
+        $data['category'] = \Timeandattendance::select('date as label')->where('date', ">=", date('Y-m-d',strtotime('last monday')))->where('instance', '=', $currentInstance )->groupBy('date')->get()->toJson();
+
+        // Get Absenteeism Data
+        $present_data = \Timeandattendance::select(DB::raw('COUNT( CASE WHEN  `attendance` = "present" THEN 0 END ) AS value'))->where('date', ">=", date('Y-m-d',strtotime('last monday')))->where('instance', '=', $currentInstance )->groupBy('date')->get();
+        $staff_present = array("seriesname" => "Staff At Work", "data" => $present_data);
+        $absent_data = \Timeandattendance::select(DB::raw('COUNT( CASE WHEN  `attendance` <> "present" THEN 0 END ) AS value'))->where('date', ">=", date('Y-m-d',strtotime('last monday')))->where('instance', '=', $currentInstance )->groupBy('date')->get();
+        $staff_absent = array("seriesname" => "Staff Not At Work", "data" => $absent_data);
+        $staff_graph = array($staff_present, $staff_absent);
+        $data['staff_graph'] = json_encode($staff_graph);
+
+        // Get Lateness Data
+        $ontime_data = \Timeandattendance::select(DB::raw('COUNT(*) as count'), DB::raw('COUNT( CASE WHEN  `punctuality` <= '.$lateness_threshold.' THEN 0 END ) AS value'))->where('date', ">=", date('Y-m-d',strtotime('last monday')))->where('instance', '=', $currentInstance )->groupBy('date')->get();
+        $staff_ontime = array("seriesname" => "Staff on Time", "data" => $ontime_data);
+        $late_data = \Timeandattendance::select(DB::raw('COUNT( CASE WHEN  `punctuality` > '.$lateness_threshold.' THEN 0 END ) AS value'))->where('date', ">=", date('Y-m-d',strtotime('last monday')))->where('instance', '=', $currentInstance )->groupBy('date')->get();
+        $staff_late = array("seriesname" => "Late Staff", "data" => $late_data);
+        $lateness_graph = array($staff_ontime, $staff_late);
+        $data['lateness_graph'] = json_encode($lateness_graph);
+
+        // Get Lateness Data
+        $proximal_data = \Timeandattendance::select(DB::raw('COUNT( CASE WHEN  `proximity` >= '.$proximity_target.' THEN 0 END ) AS value'))->where('date', ">=", date('Y-m-d',strtotime('last monday')))->where('instance', '=', $currentInstance )->groupBy('date')->get();
+        $staff_proximal = array("seriesname" => "Staff At Work", "data" => $proximal_data);
+        $away_data = \Timeandattendance::select(DB::raw('COUNT( CASE WHEN  `proximity` < '.$proximity_target.' THEN 0 END ) AS value'))->where('date', ">=", date('Y-m-d',strtotime('last monday')))->where('instance', '=', $currentInstance )->groupBy('date')->get();
+        $staff_away = array("seriesname" => "Staff Not At Work", "data" => $away_data);
+        $staff_wsproximity = array($staff_proximal, $staff_away);
+        $data['wsproximity_graph'] = json_encode($staff_wsproximity);
+        $data['report_period']      =   "Report for ".date('Y-m-d',strtotime('last monday'))." to ". date('Y-m-d');
+        $data['report_name_date']   =   "day_".date('Y-m-d',strtotime('last monday'))."_".date('Y-m-d');
+
+        return \View::make('hiptna.showdashboard')->with('data', $data);
     }
-
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		//
-	}
-
-
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
-
-
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
-
-
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
-
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
-
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
-
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
-
 
 }
