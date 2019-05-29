9ba86670617aa78eb20e2b14ca8b253c0a9b9913
Hannes <hannesvantonder@crossplatforms.co.za>
2019-05-29 20:38:59 +0200

    added updates

--- app/models/Picknpay.php
+++ app/models/Picknpay.php
@@ -28,7 +28,7 @@ class Picknpay extends Eloquent {
     public static function getChartDwellTimeData(){
         //should return array
         //TODO: Pass store name in here and filter according to store.
-        return Picknpay::orderBy('id', 'ASC')->select('dwell_time')->get()->groupBy('category')->sum();
+        return Picknpay::orderBy('id', 'ASC')->select('dwell_time, category')->get()->groupBy('category')->sum();
 
         // return Picknpay::orderBy('id', 'ASC')->get()->groupBy('category')->map(function($row) {
         //     return $row->sum('dwell_time');
