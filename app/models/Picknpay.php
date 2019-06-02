70d9e0aac20f3254724cda4ad58c0323c933b481
Hannes <hannesvantonder@crossplatforms.co.za>
2019-06-02 21:44:46 +0200

    added changes

--- app/models/Picknpay.php
+++ app/models/Picknpay.php
@@ -47,7 +47,7 @@ class Picknpay extends Eloquent {
         $startDate = $dateRange['startDate'];
         $endDate = $dateRange['endDate'];
 
-        $test = '';
+        $prevDate = '';
 
         $data = Picknpay::orderBy('id', 'ASC')
         ->select('category', 'created_at')
@@ -55,10 +55,16 @@ class Picknpay extends Eloquent {
         ->where('created_at', "<=", $endDate)
         ->groupBy('category')
         ->get()->map(function($row) {
-            return array('label' => $row['created_at']->toDateString());
-        })->filter(function ($row) {
-            $test = 'asdf';
-            return $row;
+
+            if ($prevDate == $row['created_at']){
+                return null;
+            }
+            else {
+                return array('label' => $row['created_at']->toDateString());
+            }
+
+            $prevDate = $row['created_at'];
+
         });
 
 
