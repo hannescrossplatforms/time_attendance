11108425c28a5eccc3459854ded0a240b6baf2d2
Hannes <hannesvantonder@crossplatforms.co.za>
2019-06-02 22:09:11 +0200

    added changes

--- app/models/Picknpay.php
+++ app/models/Picknpay.php
@@ -56,7 +56,7 @@ class Picknpay extends Eloquent {
         ->groupBy('category')
         ->get()->map(function($row) {
             return array('label' => $row['created_at']->toDateString());
-        })->toArray();
+        });
 
          return $data;
 
