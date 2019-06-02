a6b8ccc9464c35306f145cf613d8d5acbc329be6
Hannes <hannesvantonder@crossplatforms.co.za>
2019-06-02 21:00:02 +0200

    added changes

--- app/models/Picknpay.php
+++ app/models/Picknpay.php
@@ -52,10 +52,9 @@ class Picknpay extends Eloquent {
         ->where('created_at', ">=", $startDate)
         ->where('created_at', "<=", $endDate)
         ->groupBy('category')
-        ->groupBy('created_at')
         ->get()->map(function($row) {
             return array('label' => $row['created_at']->toDateString());
-        });
+        }).unique();
 
     }
 
