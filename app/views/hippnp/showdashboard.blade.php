72691c86b0ee93b3702010801a3e3ad7f93b305a
hannescrossplatforms <hannesvantonder@crossplatforms.co.za>
2019-06-05 18:52:50 +0200

    asdf

--- app/views/hippnp/showdashboard.blade.php
+++ app/views/hippnp/showdashboard.blade.php
@@ -267,10 +267,9 @@ function renderCharts(time, start, end) {
                 dataSource: {
                     "chart": chartProperties,
                     "categories": [{
-                        "category": <?php echo data['category']; ?>
+                        "category": data['category']
                     }],
-                    "dataset": <?php echo data['staff_graph']; ?>
-
+                    "dataset": data['staff_graph']
                 }
             });
             apiChart.render();
@@ -293,9 +292,9 @@ function renderCharts(time, start, end) {
                 dataSource: {
                     "chart": chartProperties,
                     "categories": [{
-                        "category": <?php echo data['category_avg']; ?>
+                        "category": <?php echo $data['category_avg']; ?>
                     }],
-                    "dataset": <?php echo data['staff_graph_avg']; ?>
+                    "dataset": <?php echo $data['staff_graph_avg']; ?>
 
                 }
             });
