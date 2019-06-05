40bd5eda226a7b65e58cdf8e1488fc642c4a583b
hannescrossplatforms <hannesvantonder@crossplatforms.co.za>
2019-06-05 18:36:30 +0200

    asdf

--- app/controllers/hippnp/HippnpController.php
+++ app/controllers/hippnp/HippnpController.php
@@ -37,23 +37,12 @@ class HippnpController extends \BaseController {
         $period = Input::get('period');
 
         $data['category'] = \Picknpay::chartCategoriesAsJson($period);
-        $data['staff_graph'] = \Picknpay::getChartDwellTimeData($period);
+        $data['staff_graph'] = \Picknpay::getChartTotalDwellTimeData($period);
         $data['staff_graph_avg'] = \Picknpay::getChartAverageDwellTimeData($period);
 
+        $data['report_period'] = $period;
 
-
-        // $data = array() ;
-        // $data['url'] = 'http://' . $_SERVER['SERVER_NAME'].'/';
-        // $data['customer_in_store_today'] = \Picknpay::customerInStoreToday();
-        // $data['customer_in_store_this_month'] = \Picknpay::customerInStoreThisMonth();
-        // $data['category'] = \Picknpay::chartCategoriesAsJson('rep7day');
-        // $data['staff_graph'] = \Picknpay::getChartTotalDwellTimeData('rep7day');
-        // $data['category_avg'] = \Picknpay::chartCategoriesAsJson('rep7day');
-        // $data['staff_graph_avg'] = \Picknpay::getChartAverageDwellTimeData('rep7day');
-
-        // $data['report_period'] = 'rep7day';
-
-        $json = json_encode($period);
+        $json = json_encode($data);
 
         print_r($json);
 
