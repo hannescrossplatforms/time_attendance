1615eaab6431d82964c21d7a28826e83e23e609f
hannescrossplatforms <hannesvantonder@crossplatforms.co.za>
2019-06-09 21:21:10 +0200

    changes

--- app/models/Picknpay.php
+++ app/models/Picknpay.php
@@ -80,7 +80,7 @@ class Picknpay extends Eloquent {
         // return $date;
 
         return Picknpay::select(DB::raw('sum(CAST(dwell_time AS UNSIGNED)) value'))
-        ->where(DB::raw("DATE_FORMAT(created_at, '%Y-%m-%d') = '$date 00:00:00'"))
+        ->where(DB::raw("DATE_FORMAT(created_at, '%Y-%m-%d') = '$date'"))
         ->groupBy('category', 'created_at')
         ->orderBy('created_at')
         ->get();
