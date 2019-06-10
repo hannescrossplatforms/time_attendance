10db2f92f15cef746bddba79c9e72eb1924f25fb
hannescrossplatforms <hannesvantonder@crossplatforms.co.za>
2019-06-10 23:42:31 +0200

    changes

--- app/models/Picknpay.php
+++ app/models/Picknpay.php
@@ -45,10 +45,10 @@ class Picknpay extends Eloquent {
     }
 
     public static function fetchCategoryPerDate($dates, $category){
-        $formatted_dates = $dates->map(function($date) {
+        $formatted_dates = .implode(",",$dates->map(function($date) {
             return [$date['label']];
         })
-        ->toArray().join(',');
+        ->toArray());
         return DB::select(DB::raw("SELECT sum(CAST(dwell_time AS UNSIGNED))AS value FROM picknpay WHERE DATE_FORMAT(created_at,'%Y-%m-%d') IN ($formatted_dates) AND category = '$category' GROUP BY DATE_FORMAT(created_at,'%Y-%m-%d') ORDER BY DATE_FORMAT(created_at,'%Y-%m-%d')"));
 
     }
