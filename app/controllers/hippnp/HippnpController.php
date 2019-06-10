414f978554b757c51ba2fc2645b7e10a25c254e1
hannescrossplatforms <hannesvantonder@crossplatforms.co.za>
2019-06-10 22:18:25 +0200

    changes

--- app/controllers/hippnp/HippnpController.php
+++ app/controllers/hippnp/HippnpController.php
@@ -57,18 +57,18 @@ class HippnpController extends \BaseController {
                 $dates_series = array();
             }
             $dates_series = array_push($dates_series, $date['created_att']);
-            foreach ( $categories as $category ) {
-                // $staff_graph = array_push({value: $staff_graph, });
-                console.info($category);
-                $obj[] = [
-                    'seriesname' => $category,
-                    'data' => \Picknpay::fetchCategoryPerDate($date['created_att'], $category)
-                ];
-                $my_asshole = array_push($my_asshole, $obj);
+            // foreach ( $categories as $category ) {
+            //     // $staff_graph = array_push({value: $staff_graph, });
+            //     $obj[] = [
+            //         'seriesname' => $category,
+            //         'data' => \Picknpay::fetchCategoryPerDate($date['created_att'], $category)
+            //     ];
+            //     $my_asshole = array_push($my_asshole, $obj);
 
 
 
-            }
+            // }
+            $data['asdf'] = $category;
         // });
         };
 
