aaf1a1b0596dc7fa81d87f413e4e5536dd178cc9
hannescrossplatforms <hannesvantonder@crossplatforms.co.za>
2019-06-11 00:59:55 +0200

    changes

--- app/controllers/hippnp/HippnpController.php
+++ app/controllers/hippnp/HippnpController.php
@@ -78,7 +78,7 @@ class HippnpController extends \BaseController {
 
                 foreach ( $dates as $date ) {
                     // dataArray
-                    $response = \Picknpay::fetchCategoryPerDate($data['created_att'], $categoryName);
+                    $response = \Picknpay::fetchCategoryPerDate($date['created_att'], $categoryName);
                     $dataArray = array_push($dataArray, $response->value);
 
                 }
