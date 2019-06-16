b95163433f7a6d54325f63760a78821d23b89907
hannescrossplatforms <hannesvantonder@crossplatforms.co.za>
2019-06-16 22:24:12 +0200

    changes

--- app/controllers/hippnp/HippnpController.php
+++ app/controllers/hippnp/HippnpController.php
@@ -21,8 +21,7 @@ class HippnpController extends \BaseController {
         $data['customer_in_store_today'] = \Picknpay::customerInStoreToday();
         $data['customer_in_store_this_month'] = \Picknpay::customerInStoreThisMonth();
 
-
-        self.periodchartJsondata('rep7day');
+        $this->periodchartJsondata('rep7day');
 
 
         $data['category'] = \Picknpay::chartCategoriesAsJson('rep7day', false);
