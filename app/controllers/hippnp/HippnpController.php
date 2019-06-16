c4196b500923776053b0cb57b3c5d2281f9fbd5a
hannescrossplatforms <hannesvantonder@crossplatforms.co.za>
2019-06-16 22:31:42 +0200

    changes

--- app/controllers/hippnp/HippnpController.php
+++ app/controllers/hippnp/HippnpController.php
@@ -13,7 +13,7 @@ use Session;
 
 class HippnpController extends \BaseController {
 
-	public function showDashboard(){
+	public static function showDashboard(){
 
         $data = array() ;
 
@@ -21,7 +21,90 @@ class HippnpController extends \BaseController {
         $data['customer_in_store_today'] = \Picknpay::customerInStoreToday();
         $data['customer_in_store_this_month'] = \Picknpay::customerInStoreThisMonth();
 
-        $this->periodchartJsondata('rep7day', $data);
+
+
+//////////
+        $allCategories = \Picknpay::fetchAllCategories($period);
+        $dates = \Picknpay::datesToFetchChartDataFor($period)
+        ->map(function($row) {
+                return ['label' => $row['created_att']];
+            });
+
+        $data['category_list'] = $dates;
+
+        // Sum of all categories
+
+        $finalChartObject = array();
+
+        foreach ($allCategories as $category) {
+            $categoryName = $category->category;
+            $dataArray = array();
+
+            foreach ( $dates as $date ) {
+                $response = \Picknpay::fetchDwellTimeDataForCategoryWithDate($date['label'], $categoryName, "SUM");
+                if (count($response) == 0) {
+                    $empty_array = array(['value' => '0']);
+                    array_push($dataArray, $empty_array);
+                } else {
+                    array_push($dataArray, $response);
+                }
+
+            }
+
+            $obj[] = [
+                'seriesname' => $categoryName,
+                'data' => $dataArray
+            ];
+            array_push($finalChartObject, $obj);
+        };
+
+        if (count($finalChartObject) > 0) {
+            $data['category_list_data'] = $finalChartObject[count($finalChartObject)- 1];
+        }
+        else {
+            $data['category_list_data'] = null;
+        }
+
+        $obj = null;
+
+        // Average of all categories
+
+        $finalAverageChartObject = array();
+
+        foreach ($allCategories as $category) {
+            $categoryName = $category->category;
+            $dataArrayAvg = array();
+
+            foreach ( $dates as $date ) {
+                $response = \Picknpay::fetchDwellTimeDataForCategoryWithDate($date['label'], $categoryName, "AVG");
+                if (count($response) == 0) {
+                    $empty_array = array(['value' => '0']);
+                    array_push($dataArrayAvg, $empty_array);
+                } else {
+                    array_push($dataArrayAvg, $response);
+                }
+
+            }
+
+            $obj[] = [
+                'seriesname' => $categoryName,
+                'data' => $dataArrayAvg
+            ];
+            array_push($finalAverageChartObject, $obj);
+        };
+
+        if (count($finalAverageChartObject) > 0) {
+            $data['category_list_data_average'] = $finalAverageChartObject[count($finalAverageChartObject)- 1];
+        }
+        else {
+            $data['category_list_data_average'] = null;
+        }
+
+        $obj = null;
+
+        // $json = json_encode($data);
+
+/////////
 
 
         $data['category'] = \Picknpay::chartCategoriesAsJson('rep7day', false);
@@ -35,15 +118,11 @@ class HippnpController extends \BaseController {
 
     }
 
-    public function periodchartJsondata($timePeriod = 'empty', $data){
+    public function periodchartJsondata(){
 
-        if ($timePeriod == 'empty') {
-            $period = Input::get('period');
-        }
-        else {
-            $period = $timePeriod;
-        }
+        $data = array();
 
+        $period = Input::get('period');
         $allCategories = \Picknpay::fetchAllCategories($period);
         $dates = \Picknpay::datesToFetchChartDataFor($period)
         ->map(function($row) {
@@ -124,11 +203,7 @@ class HippnpController extends \BaseController {
 
         $json = json_encode($data);
 
-
-        if ($timePeriod == 'empty') {
-            print_r($json);
-        }
-
+        print_r($json);
 
     }
 
