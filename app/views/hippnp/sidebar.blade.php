ba7b0cec11c59288a0105b901704c1de1d192c5d
hannescrossplatforms <hannesvantonder@crossplatforms.co.za>
2019-06-18 06:51:17 +0200

    changes

--- app/views/hippnp/sidebar.blade.php
+++ app/views/hippnp/sidebar.blade.php
@@ -5,34 +5,33 @@
     </div>
     <div class="productTitle pull-left">
         <h2><strong>T&A</strong></h2>
-        <h1>Test</h1>
     </div>
     <div class="clearfix"></div>
     <ul class="nav nav-sidebar nav-products">
-        @if (\User::hasAccess("superadmin"))
+        <!-- @if (\User::hasAccess("superadmin")) -->
         <li class="li-wifi"><a href="{{ url('hipwifi_showdashboard'); }}"><i class="fa fa-wifi"></i></a></li>
-        @endif
-        @if (\User::hasAccess("superadmin"))
+        <!-- @endif -->
+        <!-- @if (\User::hasAccess("superadmin")) -->
         <li class="li-rm" class="active"> <a href="{{ url('hiprm_showdashboard'); }}"><i class="fa fa-credit-card"></i></a> </li>
-        @endif
-        @if (\User::hasAccess("superadmin"))
+        <!-- @endif -->
+        <!-- @if (\User::hasAccess("superadmin")) -->
         <li class="li-jam"><a href="{{ url('hipjam_showdashboard'); }}"><i class="fa fa-shopping-cart"></i></a></li>
-        @endif
-        @if (\User::hasAccess("superadmin"))
+        <!-- @endif -->
+        <!-- @if (\User::hasAccess("superadmin")) -->
         <li class="li-engage"><a href="{{ url('hipengage_showevents'); }}"><i class="fa fa-bullhorn"></i></a></li>
-        @endif
-        @if (\User::hasAccess("superadmin"))
+        <!-- @endif -->
+        <!-- @if (\User::hasAccess("superadmin")) -->
         <li class="li-reports"><a href="{{ url('hipreports_showdashboard'); }}"><i class="fa fa-bar-chart"></i></a></li>
-        @endif
-        @if (\User::hasAccess("superadmin"))
+        <!-- @endif -->
+        <!-- @if (\User::hasAccess("superadmin")) -->
         <li class="li-tna"><a href="{{ url('hiptna_showdashboard'); }}"><i class="fa fa-clock-o fa-3x"></i></a></li>
-        @endif
-        @if (\User::hasAccess("superadmin"))
+        <!-- @endif -->
+        <!-- @if (\User::hasAccess("superadmin")) -->
         <li class="li-adm"><a href="{{ url('admin_showdashboard'); }}"><i class="fa fa-gear fa-3x"></i></a></li>
-        @endif
-        @if (\User::hasAccess("admin"))
+        <!-- @endif -->
+        <!-- @if (\User::hasAccess("admin")) -->
 
-        @endif
+        <!-- @endif -->
         <li><a href="{{ url('support'); }}"><i class="fa fa-phone"></i></a></li>
         <li><a href="{{ url('logout'); }}"><i class="fa fa-unlock"></i></a></li>
     </ul>
