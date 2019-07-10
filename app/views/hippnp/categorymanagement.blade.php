f53f950ee2b706b0e58497ff7483c1734332232e
hannescrossplatforms <hannesvantonder@crossplatforms.co.za>
2019-07-10 12:47:24 +0200

    added a menu item

--- app/views/hippnp/categorymanagement.blade.php
+++ app/views/hippnp/categorymanagement.blade.php
@@ -34,7 +34,7 @@
 
 
                     <h1>
-                    $data
+                    {{$data}}
                     </h1>
 
                     <?php foreach($data['venues'] as $key=>$value): ?>
