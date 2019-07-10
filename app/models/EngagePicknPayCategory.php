7c0cb55d76e31a57ee9f35d712f9bb4e65c83eba
hannescrossplatforms <hannesvantonder@crossplatforms.co.za>
2019-07-10 14:18:00 +0200

    added a menu item

--- app/models/EngagePicknPayCategory.php
+++ app/models/EngagePicknPayCategory.php
@@ -2,28 +2,9 @@
 
 class EngagePicknPayCategory extends Eloquent {
 
-
-
-
     protected $connection = 'hipengage';
 
-    
     public function getCategories($id) {
         return $this::where('store_id', $id);
     }
 }
-
-
-// protected $connection = 'hipengage';
-// public function __construct() {
-//     $this->connection = \Utils::getEngageDbConnection();
-// }
-
-// public function events() {
-//     return $this->hasMany('App\Hipevent', 'engagebrand_code', 'code');
-// }
-
-// public function devices() {
-//     return $this->hasMany('App\Device', 'engagebrand_code', 'code');
-// }
-
