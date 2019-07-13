7e740e9edbcc51abd81fcca0120c6909e0ddcdbd
hannescrossplatforms <hannesvantonder@crossplatforms.co.za>
2019-07-13 17:54:41 +0200

    added updates

--- app/routes.php
+++ app/routes.php
@@ -348,7 +348,7 @@ Route::get('hippnp/periodchartJsondata','hippnp\HippnpController@periodchartJson
 Route::get('hippnp/picknpay_category_management','hippnp\HippnpController@picknpayCategoryManagement');
 Route::get('hippnp/picknpay_manage_store_categories/{id}','hippnp\HippnpController@picknpayStoreCategoryManagement');
 Route::get('hippnp/picknpay_manage_store_categories/add_category/{id}','hippnp\HippnpController@addCategoryToStore');
-Route::any('/hippnp/picknpay_manage_store_categories/remove_category/{id}/{store_id}', array('uses' => 'hippnp\HippnpController@removeCategoryFromStore', 'as' => 'hippnp_remove_category'))->before('auth');
+Route::get('/hippnp/picknpay_manage_store_categories/remove_category/{id}/{store_id}', array('uses' => 'hippnp\HippnpController@removeCategoryFromStore', 'as' => 'hippnp_remove_category'))->before('auth');
 // Route::get('hippnp/picknpay_manage_store_categories/remove_category/{id}/{store_id}','hippnp\HippnpController@removeCategoryFromStore', 'as' => 'hippnp_remove_category');
 Route::post('hippnp/save_category','hippnp\HippnpController@saveCategoryToStore');
 
