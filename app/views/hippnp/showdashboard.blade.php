be4b6ccc89ab665573e749b1e233d01f974a63f7
hannescrossplatforms <hannesvantonder@crossplatforms.co.za>
2019-08-05 05:59:39 +0200

    added changes

--- app/views/hippnp/showdashboard.blade.php
+++ app/views/hippnp/showdashboard.blade.php
@@ -145,10 +145,12 @@
                             <div class="col-md-4" style="width:57%;padding:0px 0px 0px 0px;">
                                 <select id="brandcateogory" onchange="change_report_period()" class="form-control"
                                     name="brandcateogory">
-                                    <option value="rep7day">This Week</option>
-                                    <option value="repthismonth">This month</option>
-                                    <option value="replastmonth">Last month</option>
-                                    <option value="daterange">Custom range</option>
+                                    <option>Select</option>
+                                    @foreach($data['all_provinces'] as $province)
+                                    <option value="{{ $province->id }}">
+                                    {{ $province->name }}
+                                    </option>
+                                    @endforeach
                                 </select>
                             </div>
                         </div>
