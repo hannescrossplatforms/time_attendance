9a8a0476ccd085deba7c27895395aa2dbb2aa788
hannescrossplatforms <hannesvantonder@crossplatforms.co.za>
2019-08-05 05:37:55 +0200

    added changes

--- app/views/hippnp/showdashboard.blade.php
+++ app/views/hippnp/showdashboard.blade.php
@@ -100,6 +100,24 @@
                     <div class="row">
                         <div class="col-md-4" style="width:30%;">
                             <div class="col-md-4" style="width:43%; padding:6px 0px 0px 0px;">
+                                <label>Store</label>
+                            </div>
+                            <div class="col-md-4" style="width:57%;padding:0px 0px 0px 0px;">
+                                <select id="brandcateogory" onchange="change_report_period()" class="form-control"
+                                    name="brandcateogory">
+                                    <option value="rep7day">This Week</option>
+                                    <option value="repthismonth">This month</option>
+                                    <option value="replastmonth">Last month</option>
+                                    <option value="daterange">Custom range</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+
+                    <br>
+                    <div class="row">
+                        <div class="col-md-4" style="width:30%;">
+                            <div class="col-md-4" style="width:43%; padding:6px 0px 0px 0px;">
                                 <label>Category</label>
                             </div>
                             <div class="col-md-4" style="width:57%;padding:0px 0px 0px 0px;">
@@ -112,7 +130,25 @@
                                 </select>
                             </div>
                         </div>
+                    </div>
+
+                    <br>
+                    <div class="row">
+                        <div class="col-md-4" style="width:30%;">
+                            <div class="col-md-4" style="width:43%; padding:6px 0px 0px 0px;">
+                                <label>Province</label>
+                            </div>
+                            <div class="col-md-4" style="width:57%;padding:0px 0px 0px 0px;">
+                                <select id="brandcateogory" onchange="change_report_period()" class="form-control"
+                                    name="brandcateogory">
+                                    <option value="rep7day">This Week</option>
+                                    <option value="repthismonth">This month</option>
+                                    <option value="replastmonth">Last month</option>
+                                    <option value="daterange">Custom range</option>
+                                </select>
+                            </div>
                         </div>
+                    </div>
                     <br><br>
                     <div id="fusion-chart">
                         <div class="row">
