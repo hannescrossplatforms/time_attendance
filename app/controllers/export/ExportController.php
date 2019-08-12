dac01d2c46f80d043cabb3a5e48f8eac5402b980
hannescrossplatforms <hannesvantonder@crossplatforms.co.za>
2019-08-12 16:43:14 +0200

    added updates

--- app/controllers/export/ExportController.php
+++ app/controllers/export/ExportController.php
@@ -15,30 +15,31 @@ class ExportController extends Controller {
 
 	public function exportVicinityAsJSON(){
 
-        // $authorization = \Input::all();
-        // $authorization = $request->all();
-
-
-
         $request = \Request::all();
         $authorization = $request['Authorization'];
-        // 328149511491BC7764417BB3D29C8
+
+        if($authorization != "328149511491BC7764417BB3D29C8")
+        {
+            return response()->json(null, 403);
+        }
+        else {
+            $data = array();
+            $vicinity = \Brand::where('name', '=', 'VICINITY')->firstOrFail();
+
+            $data['vicinity'] = $vicinity;
+            $data['vicinity']['users'] = $vicinity->users;
+            $data['vicinity']['media'] = $vicinity->medias;
+            $data['vicinity']['venues'] = $vicinity->venues;
+            $data['vicinity']['isp'] = $vicinity->isp;
+            $data['vicinity']['country'] = $vicinity->countrie;
+            $data['header'] = $authorization;
+
+            $json = json_encode($data);
+
+            print_r($json);
+        }
 
 
-        $data = array();
-        $vicinity = \Brand::where('name', '=', 'VICINITY')->firstOrFail();
-
-        $data['vicinity'] = $vicinity;
-        $data['vicinity']['users'] = $vicinity->users;
-        $data['vicinity']['media'] = $vicinity->medias;
-        $data['vicinity']['venues'] = $vicinity->venues;
-        $data['vicinity']['isp'] = $vicinity->isp;
-        $data['vicinity']['country'] = $vicinity->countrie;
-        $data['header'] = $authorization;
-
-        $json = json_encode($data);
-
-        print_r($json);
     }
 
 
