a3f9136d85f8c45f4c539572df376efcb334c62b
hannescrossplatforms <hannesvantonder@crossplatforms.co.za>
2019-08-15 10:42:21 +0200

    added updates

--- app/views/hipbidvest/checklistmanagement.blade.php
+++ app/views/hipbidvest/checklistmanagement.blade.php
@@ -72,6 +72,12 @@
     </div>
 </body>
 
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script type="text/javascript" src="/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="/js/prefixfree.min.js"></script>
+    <script src="/js/jquery.timepicker.min.js"></script>
+
 <script>
 
 
