e31aaef301a44a05f9fcc61b833e612178d7eab4
hannescrossplatforms <hannesvantonder@crossplatforms.co.za>
2019-08-15 17:25:10 +0200

    adde dupdates

--- app/views/hipbidvest/bidvestchecklisttableview.blade.php
+++ app/views/hipbidvest/bidvestchecklisttableview.blade.php
@@ -33,8 +33,8 @@ $(".table_view_item").on('click', function(){
             type: 'post',
             dataType: 'html',
             data: {
-                'room_id': listRoomID,
-                'item_id': itemID
+                'room_id': $listRoomID,
+                'item_id': $itemID
             },
             success: function(result) {
                 $("#table-container").html(result);
