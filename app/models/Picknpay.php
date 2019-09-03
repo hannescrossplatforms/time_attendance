09569e0a30c244934ae7ba41f6f741f6b6b99f70
hannescrossplatforms <hannesvantonder@crossplatforms.co.za>
2019-09-03 06:15:40 +0200

    added updates

--- app/models/Picknpay.php
+++ app/models/Picknpay.php
@@ -131,7 +131,7 @@ class Picknpay extends Eloquent {
         // ->where('created_at', "<=", $endDate)
         // ->count();
 
-        $query = Picknpay::where('creataaasdffdsaed_at', ">=", $startDate)
+        $query = Picknpay::where('created_at', ">=", $startDate)
         ->where('created_at', "<=", $endDate)
         ->groupBy('staff_id')
         ->get();
