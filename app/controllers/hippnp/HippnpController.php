75d3ed14c51074f286d3bde16bf38dbc0b6ad90f
hannescrossplatforms <hannesvantonder@crossplatforms.co.za>
2019-09-08 10:13:52 +0200

    added updates

--- app/controllers/hippnp/HippnpController.php
+++ app/controllers/hippnp/HippnpController.php
@@ -47,7 +47,7 @@ class HippnpController extends \BaseController {
 
             foreach ( $datesForAllStaff as $date ) {
 
-                $response = \Picknpay::fetchDwellTimeDataForStaffWithDate($date['label'], $stafId, $storeId, $provinceId, "SUM");
+                $response = \Picknpay::fetchDwellTimeDataForStaffWithDate($date['label'], $stafId, "", "", "SUM");
                 if (count($response) == 0) {
                     $empty_array = array(['value' => '0']);
                     array_push($dataArray, $empty_array);
