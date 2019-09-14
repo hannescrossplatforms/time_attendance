dcb83b00abcd698552acfd811e1187f0e9074f18
hannescrossplatforms <hannesvantonder@crossplatforms.co.za>
2019-09-14 14:18:05 +0200

    added updates

--- app/views/hippnp/showstaffdata.blade.php
+++ app/views/hippnp/showstaffdata.blade.php
@@ -23,6 +23,7 @@
 
         <div class="col-sm-9 col-sm-offset-3 col-md-9 col-md-offset-3 main">
           <h1 class="page-header">Staff Lookup</h1>
+          <input type="hidden" id="url" name="" value={{$data['url']}}>
 
             <div class="row">
                 <div class="col-md-4" style="width:30%;">
