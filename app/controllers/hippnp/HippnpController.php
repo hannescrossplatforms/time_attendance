6b98f1e3c49bf7821d8e92877955d1d5f1581d70
hannescrossplatforms <hannesvantonder@crossplatforms.co.za>
2019-09-15 11:21:28 +0200

    added updates

--- app/controllers/hippnp/HippnpController.php
+++ app/controllers/hippnp/HippnpController.php
@@ -237,7 +237,6 @@ class HippnpController extends \BaseController {
                     array_push($dataArrayVisitsStore, $response);
                 }
 
-            }
 
             $obj[] = [
                 'seriesname' => $storeName,
@@ -424,13 +423,13 @@ class HippnpController extends \BaseController {
     }
 
     public function periodchartJsondataSingleStaffAjax($id){
-
+        $tesy = Input::get('date');
         $period = 'today';
         $data = array() ;
         $data['currentMenuItem'] = "Dashboard";
         $data['report_period'] = 'today';
         $data['url'] = 'http://' . $_SERVER['SERVER_NAME'].'/';
-
+        $data['test'] = $tesy;
         $finalChartObjectStaff = array();
 
         $timeList = array();
