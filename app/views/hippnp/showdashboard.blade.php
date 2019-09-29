05c4e7079062b5ba300b70f6f34c4555c7f882eb
hannescrossplatforms <hannesvantonder@crossplatforms.co.za>
2019-09-29 11:39:05 +0200

    added updates

--- app/views/hippnp/showdashboard.blade.php
+++ app/views/hippnp/showdashboard.blade.php
@@ -46,7 +46,7 @@
                                         <h3>Staff In Store Today</h3>
                                     </div>
                                     <div id="staff_today" class="modStatspan">
-                                        <span style="font-size: 30%;">{{$data['customer_in_store_today']}}</span>
+                                        <span>{{$data['customer_in_store_today']}}</span>
                                     </div>
                                 </div>
                             </div>
@@ -57,8 +57,9 @@
                                     <div class="modstattitlepnp">
                                         <h3>Staff In Store This Month</h3>
                                     </div>
-                                    <div id="staff_today" class="modStatspan"><span
-                                            style="font-size: 30%;">{{$data['customer_in_store_this_month']}}</span>
+                                    <div id="staff_today" class="modStatspan"><span>
+                                    {{$data['customer_in_store_this_month']}}
+                                    </span>
                                     </div>
                                 </div>
                             </div>
