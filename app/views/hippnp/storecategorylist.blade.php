984083d9d40be4a058674c93afaad63ed1f61a72
hannescrossplatforms <hannesvantonder@crossplatforms.co.za>
2019-10-02 06:50:18 +0200

    added updates

--- app/views/hippnp/storecategorylist.blade.php
+++ app/views/hippnp/storecategorylist.blade.php
@@ -51,24 +51,6 @@
 
                     </div>
 
-
-                    <div class="table-responsive">
-                        <table class="table table-striped">
-                        <tr>
-                            <th>Category Name</th>
-                            <th>Actions</th>
-                        </tr>
-                        @foreach ($data['engageCategories'] as $category)
-                        <tr>
-                            <td>{{ $category->name }}</td>
-                            <td>
-                            <a class="btn btn-default btn-sm" href="{{route('hippnp_remove_category', ['id' => $category->id])}}">Remove</a>
-                            </td>
-                        </tr>
-                        @endforeach
-
-                        </table>
-                    </div>
                 </div>
             </div>
         </div>
