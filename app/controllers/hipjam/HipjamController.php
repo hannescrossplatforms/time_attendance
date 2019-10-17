608779cd135d61a4c028fc8efd22cb219ab27fab
hannescrossplatforms <hannesvantonder@crossplatforms.co.za>
2019-10-17 23:25:29 +0200

    added updates

--- app/controllers/hipjam/HipjamController.php
+++ app/controllers/hipjam/HipjamController.php
@@ -43,8 +43,7 @@ class HipjamController extends \BaseController
 
         foreach ($allVenues as $venue) {
 
-            echo $jsonString = file_get_contents("http://tracks03.hipzone.co.za/aggregate/$venue->id?period=now");
-            $json = json_decode($jsonString);
+            $json = json_decode(file_get_contents('http://tracks03.hipzone.co.za/aggregate/$venue->id?period=now'));
             $total = $json->total->total;
             \Log::info("[HipjamController  showDashboard] - TOTAL is: $total");
         }
