acd027c6e1aa7f1c83d67c2a29973eca93293ae2
hannescrossplatforms <hannesvantonder@crossplatforms.co.za>
2019-10-17 21:55:22 +0200

    added updates

--- app/controllers/hipjam/HipjamController.php
+++ app/controllers/hipjam/HipjamController.php
@@ -47,7 +47,7 @@ class HipjamController extends \BaseController
         // $liveNumberOfBillboardsQuery = \Venue::select(\Venue::raw("count(*) as count"))->get();
 
 
-        $liveNumberOfBillboardsQuery = \Venue::select("SELECT count(*) AS numberOfRecords FROM venues where track_type = 'billboard'")->first()->get();
+        $liveNumberOfBillboardsQuery = \Venue::select("SELECT count(*) AS numberOfRecords")->first()->get();
 
         // $liveNumberOfBillboardsQuery = \Venue::whereraw("SELECT * as count FROM venues")->count()->get();
 
