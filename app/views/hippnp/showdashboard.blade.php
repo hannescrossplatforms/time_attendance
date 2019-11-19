d6485a9794ab12fda841ac0cc99cb4f39f6ef477
hannescrossplatforms <hannesvantonder@crossplatforms.co.za>
2019-11-19 21:58:40 +0200

    added udpates

--- app/views/hippnp/showdashboard.blade.php
+++ app/views/hippnp/showdashboard.blade.php
@@ -259,7 +259,7 @@
 
 <script type="text/javascript" src="{{ asset('js/jquery.min.js') }}"></script>
 <script type="text/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
-<script type="text/javascript" src="js/hippnp.js"></script>
+<script type="text/javascript" src="/js/hippnp/hippnp.js"></script>
 
 <script src="{{ asset('js/jquery-2.1.4.js') }}"></script>
 <script src="{{ asset('js/bootstrap-datepicker.js') }}"></script>
