759896ec7b62ef7bd0c014815a17b2a17d1a3478
hannescrossplatforms <hannesvantonder@crossplatforms.co.za>
2019-11-21 20:47:22 +0200

    added updates

--- app/models/Picknpay.php
+++ app/models/Picknpay.php
@@ -248,8 +248,7 @@ class Picknpay extends Eloquent {
     public static function fetchDwellTimeDataForStaffWithinAnHour($staffID, $startDate, $endDate){
         $query = Picknpay::orderBy('created_at', 'ASC')
         ->select(DB::raw("*, IFNULL(SUM(ROUND(CAST(dwell_time AS UNSIGNED)/60)), 0) AS value"))
-        ->where('end_time', ">=", $startDate)
-        ->where('end_time', "<=", $endDate)
+        ->whereraw("end_time >= '$startDate' AND end_time <= '$endDate'")
         ->whereraw("staff_id = '$staffID'")
         ->groupBy(DB::raw("DATE_FORMAT(created_at, '%Y-%m-%d')"))
         ->orderBy(DB::raw("DATE_FORMAT(created_at, '%Y-%m-%d')"));
