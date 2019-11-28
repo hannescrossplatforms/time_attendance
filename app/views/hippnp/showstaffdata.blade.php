44b6740260327c717f27cd8e4819151f8803bbba
hannescrossplatforms <hannesvantonder@crossplatforms.co.za>
2019-11-28 21:23:18 +0200

    added updates

--- app/views/hippnp/showstaffdata.blade.php
+++ app/views/hippnp/showstaffdata.blade.php
@@ -222,6 +222,13 @@
                 },
                 success: function(data) {
                     debugger;
+
+                    $('selectedStore').children().remove();
+                    options = '<option id="" selected="selected">Please select</option>';
+                    $.each(data["storesForProvince"], function(index, value) {
+                        options = options + '<option value="' + value["id"] + '">' + value["sitename"] + '</option>';
+                    });
+
                     $("#staff_select").val('0');
 
                     var chartProperties = {
