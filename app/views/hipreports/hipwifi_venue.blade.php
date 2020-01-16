0fb9bce5103fb4a55d712680509b251bb2103f01
Hannes van Tonder <hannesvantonder@Hanness-MacBook-Pro.local>
2020-01-16 13:30:51 +0200

    added updates

--- app/views/hipreports/hipwifi_venue.blade.php
+++ app/views/hipreports/hipwifi_venue.blade.php
@@ -195,7 +195,7 @@
                         <div class="modal-content">
                           <div class="modal-header">
                             <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-                            <h6 class="modal-title" id="myModalLabel">Customer Dwell Time Percentagess</h6>
+                            <h6 class="modal-title" id="myModalLabel">Customer Dwell Time Percentages</h6>
                           </div>
                           <div class="modal-body">
                             <div class="venuerow">
