9f42022908797f96ada944866e07bc1e3bc1917b
Hannes van Tonder <hannesvantonder@Hanness-MacBook-Pro.local>
2020-01-16 15:17:04 +0200

    added updates

--- app/lib/Reports.php
+++ app/lib/Reports.php
@@ -1660,8 +1660,8 @@ class Reports extends Eloquent {
 
         // $sitename = preg_replace("/_/", " ", $nasid);
         $sitename = "KAUAI";
-        $venue = \Venue::where('sitenameasdf', 'like', 'Kauai')-get()->first();
-        
+        $venue = \Venue::where('sitename', 'like', "%$sitename%")->first();
+
         \Log::info("Hannes reports: sitename: $sitename");
 
         if ($venue) {
