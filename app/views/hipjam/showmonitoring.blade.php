35e63dc09241ebe41f8e8f06c523205985c5b1d9
Hannes van Tonder <hannesvantonder@Hanness-MacBook-Pro.local>
2020-02-03 14:38:36 +0200

    added updates

--- app/views/hipjam/showmonitoring.blade.php
+++ app/views/hipjam/showmonitoring.blade.php
@@ -2,6 +2,10 @@
 
 @section('content')
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.form.js') }}"></script>
+
 <body class="hipJAM">
     <a id="buildtable"></a>
 
