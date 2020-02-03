eee9b733d43f6b9ac0de064048245b24bb59c092
Hannes van Tonder <hannesvantonder@Hanness-MacBook-Pro.local>
2020-02-03 14:48:17 +0200

    added updates

--- app/views/hipjam/showmonitoring.blade.php
+++ app/views/hipjam/showmonitoring.blade.php
@@ -198,8 +198,8 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> -->
-    <!-- <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.css"> -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.css">
     <!-- <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.js"></script> -->
 
     <script>
