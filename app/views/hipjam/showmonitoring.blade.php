c2a12e4c4b7115c618a68d9f58e72a70f151aa5f
Hannes van Tonder <hannesvantonder@Hanness-MacBook-Pro.local>
2020-02-03 14:01:30 +0200

    added updates

--- app/views/hipjam/showmonitoring.blade.php
+++ app/views/hipjam/showmonitoring.blade.php
@@ -89,17 +89,8 @@
                                 @endif
                             @endforeach
                     </div>
-                </div>
-                
-		    </div>	
-		</div>
-		<div>
-            
-                <div id="viewVenueModalsTable"></div>
-        </div>
-	</div>
-</div>
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
   Launch demo modal
 </button>
 
@@ -122,6 +113,17 @@
       </div>
     </div>
   </div>
+</div>
+
+                </div>
+                
+		    </div>	
+		</div>
+		<div>
+            
+                <div id="viewVenueModalsTable"></div>
+        </div>
+	</div>
 </div>
 <!-- <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
