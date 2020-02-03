56b58fa98ab7f58dcf766f927dce880f03a69b9d
Hannes van Tonder <hannesvantonder@Hanness-MacBook-Pro.local>
2020-02-03 14:49:39 +0200

    added updates

--- app/views/hipjam/showmonitoring.blade.php
+++ app/views/hipjam/showmonitoring.blade.php
@@ -201,6 +201,8 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.css">
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.js"></script>
+    <script src="js/bootstrap.min.js"></script> 
+    <script src="js/prefixfree.min.js"></script>
 
     <script>
 
