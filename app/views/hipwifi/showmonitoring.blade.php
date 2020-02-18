5d3e1bf6fc5e35a173f8b53aaa3ceebc49b4eb19
Hannes van Tonder <hannesvantonder@Hanness-MBP.lan>
2020-02-18 18:00:43 +0200

    added updates

--- app/views/hipwifi/showmonitoring.blade.php
+++ app/views/hipwifi/showmonitoring.blade.php
@@ -8,51 +8,53 @@
     <div class="row">
       @include('hipwifi.sidebar')
         <div class="col-sm-9 col-sm-offset-3 col-md-9 col-md-offset-3 main">
-          	<h1 class="page-header">Venue Monitorings</h1>
+          	<h1 class="page-header">Venue Monitoring</h1>
+            <div id="page-replace-div">
+              <div class="form-group monitoring-form">
+                  <form class="form-inline" role="form" style="margin-bottom: 15px;">
+                      <label  class="sr-only" for="exampleInputEmail2">Site Name</label>
+                      <input type="text" class="form-control" id="src-sitename" placeholder="Site Name">
+                      <button id="filter" type="submit" class="btn btn-primary">Filter</button>
+                      <button id="reset" type="submit" class="btn btn-default">Reset</button>
+                  </form>
+              </div>
 
-            <div class="form-group monitoring-form">
-                <form class="form-inline" role="form" style="margin-bottom: 15px;">
-                    <label  class="sr-only" for="exampleInputEmail2">Site Name</label>
-                    <input type="text" class="form-control" id="src-sitename" placeholder="Site Name">
-                    <button id="filter" type="submit" class="btn btn-primary">Filter</button>
-                    <button id="reset" type="submit" class="btn btn-default">Reset</button>
-                </form>
-            </div>
-
-            <div class="monitoring-icons">
-              <a href="" id="listviewicon" title="List view"><i class="fa fa-align-justify fa-3x"></i></a>
-              &nbsp;&nbsp;&nbsp;&nbsp;
-              <a href="" id="gridviewicon" title="Grid view"><i class="fa fa-th fa-3x"></i></a>
-            </div>
-            
-            <div class="table-responsive clear" id="listview">
-                <table id="venueTable" class="table table-striped"> </table>
-            </div>
-            <!-- Button trigger modal -->
-            <button style="display:none;" type="button" id="launch" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong"> Launch demo modal </button>
+              <div class="monitoring-icons">
+                <a href="" id="listviewicon" title="List view"><i class="fa fa-align-justify fa-3x"></i></a>
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <a href="" id="gridviewicon" title="Grid view"><i class="fa fa-th fa-3x"></i></a>
+              </div>
+              
+              <div class="table-responsive clear" id="listview">
+                  <table id="venueTable" class="table table-striped"> </table>
+              </div>
+              <!-- Button trigger modal -->
+              <button style="display:none;" type="button" id="launch" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong"> Launch demo modal </button>
 
 
-              <!-- Modal -->
-              <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="needed-mheader" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <h5 class="modal-title" id="needed-mheader">Tabletpos Printers</h5>
-                    </div>
-                    <div class="modal-body">
-                    </div>
-                    <div class="modal-footer">
-                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <!-- Modal -->
+                <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="needed-mheader" aria-hidden="true">
+                  <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title" id="needed-mheader">Tabletpos Printers</h5>
+                      </div>
+                      <div class="modal-body">
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                      </div>
                     </div>
                   </div>
                 </div>
+
+              <div class="table-responsive clear" id="gridview">
+                  <table id="venueGrid" class="table table-venuegrid"> </table>
+
               </div>
-
-            <div class="table-responsive clear" id="gridview">
-                <table id="venueGrid" class="table table-venuegrid"> </table>
-
+              <div class="modal" id="tabletposprinter"></div>
             </div>
-            <div class="modal" id="tabletposprinter"></div>
+            
         </div>
     </div>
   </div>
