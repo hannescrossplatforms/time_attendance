22b633f4ad006e35f09c44b08469dd8957f7ba0c
Hannes van Tonder <hannesvantonder@Hanness-MBP.lan>
2020-02-18 21:50:09 +0200

    added updates

--- app/views/partials/wifi_monitoring_page.blade.php
+++ app/views/partials/wifi_monitoring_page.blade.php
@@ -109,25 +109,30 @@
 
         function createVenueModal(id, sitename, venuedata) {
 
-          modalhtml = 
-            '<div class="modal" id="modal_' + id + '" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">\n\
-              <div class="modal-dialog">\n\
-                <div class="modal-content">\n\
-                  <div class="modal-header">\n\
-                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>\n\
-                    <h6 class="modal-title" id="myModalLabel">' + sitename + '</h6>\n\
-                  </div>\n\
-                  <div class="modal-body">\n\
-                    Status Comment : <b>' + venuedata["statuscomment"]  + '</b> <br> <br> \n\
-                    Today MB (Up/Down) : ' + venuedata["bytes"]  + ' <br> \n\
-                    Gateway IP : ' + venuedata["gateway"]  + ' <br> \n\
-                    Last Check in : ' + venuedata["lastcheckin"]  + ' <br> \n\
-                </div>\n\
-              </div>\n\
-            </div>\n\
-          </div>';
+        $('#modal_' + id).on('hidden.bs.modal', function () {
+            debugger;
+            selectedModalId = null;
+        });
 
-          return modalhtml;
+        modalhtml = 
+        '<div class="modal" id="modal_' + id + '" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">\n\
+            <div class="modal-dialog">\n\
+            <div class="modal-content">\n\
+                <div class="modal-header">\n\
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>\n\
+                <h6 class="modal-title" id="myModalLabel">' + sitename + '</h6>\n\
+                </div>\n\
+                <div class="modal-body">\n\
+                Status Comment : <b>' + venuedata["statuscomment"]  + '</b> <br> <br> \n\
+                Today MB (Up/Down) : ' + venuedata["bytes"]  + ' <br> \n\
+                Gateway IP : ' + venuedata["gateway"]  + ' <br> \n\
+                Last Check in : ' + venuedata["lastcheckin"]  + ' <br> \n\
+            </div>\n\
+            </div>\n\
+        </div>\n\
+        </div>';
+
+        return modalhtml;
 
         }
         
