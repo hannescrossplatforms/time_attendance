4b99aa69a643ec9b150fff4ccb27ebee004122dd
Hannes <hannes@crossplatforms.co.za>
2020-06-17 11:22:28 +0200

    added a test swal

--- app/views/hipwifi/showservers.blade.php
+++ app/views/hipwifi/showservers.blade.php
@@ -157,6 +157,7 @@
           if (willDelete) {
             
             swal("Deleted!", "Server has been deleted!", "success");
+            debugger;
           $.ajax({
             type: "GET",
             dataType: 'json',
