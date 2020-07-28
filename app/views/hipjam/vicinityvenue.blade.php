13427d95bd6aa3c4a1815deec44418b110c6877c
Hannes <hannes@crossplatforms.co.za>
2020-07-28 12:55:11 +0200

    added udpates

--- app/views/hipjam/vicinityvenue.blade.php
+++ app/views/hipjam/vicinityvenue.blade.php
@@ -67,17 +67,17 @@
                     <div class="col-6 text-left">
                       <div class="form-group">
                         <label for="track_slug">Reset Board UUID</label>
-                        @if ($data['venue']->track_type == 'billboard' && $data['venue']->sitename == 'SBOutdoor Meadowvale')
+                        @if ($data['venue']->track_type == 'billboard' && $data['venue']->sitename == 'SBOutdoor MeadowdaleR24')
                         <input disabled id="track_slug" type="text" class="form-control" name="track_slug" placeholder="" value="c37e6ff3-d3ed-4e63-8732-aeb26b856799">
-                        @elseif ($data['venue']->track_type == 'billboard' && $data['venue']->sitename == 'SBOutdoor Michelle Ave')
+                        @elseif ($data['venue']->track_type == 'billboard' && $data['venue']->sitename == 'SBOutdoor MichelleAve')
                         <input disabled id="track_slug" type="text" class="form-control" name="track_slug" placeholder="" value="b6296d42-8682-4137-aef6-f705ba94fbbf">
                         @elseif ($data['venue']->track_type == 'billboard' && $data['venue']->sitename == 'SBOutdoor PWC')
                         <input disabled id="track_slug" type="text" class="form-control" name="track_slug" placeholder="" value="e8fc8e24-f15a-43a9-b57f-dd034735f3dc">
-                        @elseif ($data['venue']->track_type == 'billboard' && $data['venue']->sitename == 'SBOutdoor RandView')
+                        @elseif ($data['venue']->track_type == 'billboard' && $data['venue']->sitename == 'SBOutdoor RandViewCentre')
                         <input disabled id="track_slug" type="text" class="form-control" name="track_slug" placeholder="" value="4a1290f1-9700-494f-b0c5-0f17b4fc5ca1">
-                        @elseif ($data['venue']->track_type == 'billboard' && $data['venue']->sitename == 'SBOutdoor Sephako')
+                        @elseif ($data['venue']->track_type == 'billboard' && $data['venue']->sitename == 'SBOutdoor SephakoMakgathoDr')
                         <input disabled id="track_slug" type="text" class="form-control" name="track_slug" placeholder="" value="55e9523b-1172-4dcb-96bc-549142b3ef94">
-                        @elseif ($data['venue']->track_type == 'billboard' && $data['venue']->sitename == 'SBOutdoor Stoneridge')
+                        @elseif ($data['venue']->track_type == 'billboard' && $data['venue']->sitename == 'SBOutdoor StoneridgeDrive')
                         <input disabled id="track_slug" type="text" class="form-control" name="track_slug" placeholder="" value="a4817e7e-04df-49a3-9fba-53bf2ab3e38c">
                         @endif
                       </div>
@@ -86,17 +86,17 @@
                     <div class="col-6 text-left">
                       <div class="form-group">
                         <label for="track_slug">Reset Board Auth Key</label>
-                        @if ($data['venue']->track_type == 'billboard' && $data['venue']->sitename == 'SBOutdoor Meadowvale')
+                        @if ($data['venue']->track_type == 'billboard' && $data['venue']->sitename == 'SBOutdoor MeadowdaleR24')
                         <input disabled id="track_slug" type="text" class="form-control" name="track_slug" placeholder="" value="0b8926df-7a59-4b4c-95ae-2d4d6b40c40d">
-                        @elseif ($data['venue']->track_type == 'billboard' && $data['venue']->sitename == 'SBOutdoor Michelle Ave')
+                        @elseif ($data['venue']->track_type == 'billboard' && $data['venue']->sitename == 'SBOutdoor MichelleAve')
                         <input disabled id="track_slug" type="text" class="form-control" name="track_slug" placeholder="" value="2f352d4f-d189-40a9-8477-4782a2fcb13a">
                         @elseif ($data['venue']->track_type == 'billboard' && $data['venue']->sitename == 'SBOutdoor PWC')
                         <input disabled id="track_slug" type="text" class="form-control" name="track_slug" placeholder="" value="968357ac-102b-4e99-91cf-d151e180556b">
-                        @elseif ($data['venue']->track_type == 'billboard' && $data['venue']->sitename == 'SBOutdoor RandView')
+                        @elseif ($data['venue']->track_type == 'billboard' && $data['venue']->sitename == 'SBOutdoor RandViewCentre')
                         <input disabled id="track_slug" type="text" class="form-control" name="track_slug" placeholder="" value="59dc065f-788c-47d9-8a9c-519f2e035701">
-                        @elseif ($data['venue']->track_type == 'billboard' && $data['venue']->sitename == 'SBOutdoor Sephako')
+                        @elseif ($data['venue']->track_type == 'billboard' && $data['venue']->sitename == 'SBOutdoor SephakoMakgathoDr')
                         <input disabled id="track_slug" type="text" class="form-control" name="track_slug" placeholder="" value="78bc98fa-86ab-4854-8c78-8f0869faceff">
-                        @elseif ($data['venue']->track_type == 'billboard' && $data['venue']->sitename == 'SBOutdoor Stoneridge')
+                        @elseif ($data['venue']->track_type == 'billboard' && $data['venue']->sitename == 'SBOutdoor StoneridgeDrive')
                         <input disabled id="track_slug" type="text" class="form-control" name="track_slug" placeholder="" value="b5763e8d-3e28-40c8-9af9-e48d540cd9a7">
                         @endif
                       </div>
