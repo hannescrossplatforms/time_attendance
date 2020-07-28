9b5c63e7696c3c25adc6995b67f6b967b7647780
Hannes <hannes@crossplatforms.co.za>
2020-07-28 12:55:59 +0200

    added udpates

--- app/views/hipjam/vicinityvenue.blade.php
+++ app/views/hipjam/vicinityvenue.blade.php
@@ -56,7 +56,7 @@
                     </div>
                     <div class="col-6 text-left">
                       <div class="form-group">
-                        <label for="billboard_retail">OOH Site / Retail</label>
+                        <label for="billboard_retail">OOH Site / Retails</label>
                         <select class="form-control" name="track_type" id="track_type" data-default-selected="{{$data['venue']->track_type}}">
                           <option value="venue">Venue</option>
                           <option value="billboard">OOH Site</option>
