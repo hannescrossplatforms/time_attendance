e88d23e14200abbb9d0a397cc32205ab0283c1d1
Hannes <hannes@crossplatforms.co.za>
2020-08-12 10:46:57 +0200

    aded updates

--- app/controllers/hipjam/HipjamController.php
+++ app/controllers/hipjam/HipjamController.php
@@ -2107,7 +2107,7 @@ public function getOohSiteData() {
         $sensor = new \Sensor();
         $venue = \DB::table('venues')->select("sonoff_device_uuid", "sonoff_device_auth_token", "sonoff_device_on_status", "sonoff_device_action_status", "sonoff_device_action_time")->where('id', '=', $input)->first();        
         $sensordata = $sensor->getSensorsForVenue($input);
-        $sensordata->add($venue);
+        // $sensordata->add($venue);
         return $sensordata;
     }
 
