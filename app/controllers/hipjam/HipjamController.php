76112f6357788ef1aaffe10b3b4f39869f33021f
Hannes <hannes@crossplatforms.co.za>
2020-08-28 08:17:48 +0200

    updated ewelink solution to work same as sonoff solution

--- app/controllers/hipjam/HipjamController.php
+++ app/controllers/hipjam/HipjamController.php
@@ -4518,11 +4518,14 @@ public function getOohSiteData() {
                 'error' => 'Unauthorized access'
             ],401);
         }
-
-
+        
+        
+        
         $venue = \Venue::where('sonoff_device_auth_token', '=', $device_auth_token)->first();
         $venue->sonoff_query_time = \Carbon\Carbon::now()->addHours(2);
         $venue->save();
+        // get_sonoff_status
+        \Log::info("[HipjamController  getSonoffStatus] - device querying: $venue->sitename");
 
         return Response::json([
             'success' => 'true', 'device_status' => $venue->sonoff_device_on_status
