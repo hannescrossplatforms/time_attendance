ae9aa24d73cc71524955728bf318256c1ae3f68e
Hannes <hannes@crossplatforms.co.za>
2020-10-19 12:23:34 +0200

    added updates

--- app/views/hipwifi/preview-mb.blade.php
+++ app/views/hipwifi/preview-mb.blade.php
@@ -61,13 +61,13 @@
             <ul>
                 <li>
                     <div class="merrormsg"></div>
-                    <input name="login[username]" id="login_username" type="text" placeholder="Username" />
+                    <!-- <input name="login[username]" id="login_username" type="text" placeholder="Username" /> -->
 
                 </li>
                 <BR>
                 <li>
                     <div class="merrormsg"></div>
-                    <input name="login[password]" id="login_password" type="password" placeholder="Passwords" />
+                    <input name="login[password]" id="login_password" type="password" placeholder="Password" />
                 </li>
             </ul>
 
